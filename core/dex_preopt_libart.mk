92bb71ef45c0d3fe2f76f8880ca27619218da2fd
Fang Hui <b31070@freescale.com>
2014-11-11 14:52:31 +0800

    MA-5839-2 Update android to Lollipop
    set LIBART_IMG_TARGET_BASE_ADDRESS to 0x50000000,
    or virtual space confilicted.
    
    Signed-off-by: Fang Hui <b31070@freescale.com>

--- core/dex_preopt_libart.mk
+++ core/dex_preopt_libart.mk
@@ -26,7 +26,7 @@ COMPILED_CLASSES := $(call word-colon,1,$(firstword \
 
 # start of image reserved address space
 LIBART_IMG_HOST_BASE_ADDRESS   := 0x60000000
-LIBART_IMG_TARGET_BASE_ADDRESS := 0x70000000
+LIBART_IMG_TARGET_BASE_ADDRESS := 0x50000000
 
 define get-product-default-property
 $(strip $(patsubst $(1)=%,%,$(filter $(1)=%,$(PRODUCT_DEFAULT_PROPERTY_OVERRIDES))))
